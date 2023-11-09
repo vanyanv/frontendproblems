4cc16afa90fac63ecae8b29d2d48002f64ac96a8
Vardan <vardan.vanyan@gmail.com>
2023-11-08 16:21:10 -0800

    added links to each problem

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,23 @@
+import Link from 'next/link';
+
 export default function Home() {
   return (
-    <div className="flex justify-center">
+    <div className='flex justify-center'>
       <h1>Welcome to Frontend Problems</h1>
+      <div className='flex'>
+        <div>
+          <Link href='/mortgage-calculator'> Mortgage Calculator</Link>
+        </div>
+        <div>
+          <Link href='/contact-form'> Contact Form</Link>
+        </div>
+        <div>
+          <Link href='/todo-list'> Todo List </Link>
+        </div>
+        <div>
+          <Link href='/tictactoe'> Tictactoe </Link>
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/app/tictactoe/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import TicTacToe from './TicTacToe';
+export default function page() {
+  return (
+    <div>
+      <TicTacToe />
+    </div>
+  );
+}
